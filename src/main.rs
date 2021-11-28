9fd51e41ef9e42ffbbe739cec578fea1915b80dc
shota3506 <s.shota.710.3506@gmail.com>
2021-11-28 15:34:42 +0900

    first commit

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+extern crate rsgrep;
+
+use std::env;
+use std::process;
+
+use rsgrep::Config;
+
+fn main() {
+    let config = Config::new(env::args()).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+
+    if let Err(e) = rsgrep::run(config) {
+        eprintln!("Application error: {}", e);
+        process::exit(1);
+    }
+}
